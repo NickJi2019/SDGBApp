86535b69e7225639c795de13350135333bbb63ff
NickJi <nickji2019@outlook.com>
2023-11-09 11:08:12 -0500

    init commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            // put your Multiplatform dependencies here
+        }
+    }
+}
+
+android {
+    namespace = "com.sbga.sdgbapp.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
