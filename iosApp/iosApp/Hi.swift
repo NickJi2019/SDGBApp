1fdd5a7288301a8f75ae5b0ce5e991fe41098acb
NickJi <nickji2019@outlook.com>
2023-11-28 21:36:09 -0500

    pods support
    Signed-off-by: NickJi <nickji2019@outlook.com>

---
+++ iosApp/iosApp/Hi.swift
@@ -0,0 +1,15 @@
+//
+//  Hi.swift
+//  iosApp
+//
+//  Created by Nick 嵇 on 2023-11-28.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+
+@objc class Hi{
+    @objc func sayHi(str:NSString) -> NSString {
+        return str + "Hi"
+    }
+}
