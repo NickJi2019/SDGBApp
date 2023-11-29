7faf17e5c149926e7befedf61af56a00f2ad181f
NickJi <nickji2019@outlook.com>
2023-11-28 21:33:13 -0500

    add RFC-1951 compress support to apple target
    Signed-off-by: NickJi <nickji2019@outlook.com>

---
+++ shared/src/appleMain/kotlin/com.sbga.sdgbapp/Utility/Compressor.kt
@@ -0,0 +1,57 @@
+package com.sbga.sdgbapp.Utility
+
+
+import kotlinx.cinterop.*
+import platform.posix.*
+import platform.zlib.*
+
+@OptIn(ExperimentalForeignApi::class)
+
+actual object Compressor {
+    val log = Log()
+    actual fun deflate(data: ByteArray): ByteArray {
+        memScoped{
+            val outputLength = alloc<uLongfVar>().apply {
+                value = compressBound(data.size.convert())
+            }
+            val outputData = allocArray<uBytefVar>(outputLength.value.toInt())
+            val inputData = data.toUByteArray().toCValues()
+
+            compress(outputData, outputLength.ptr, inputData, data.size.convert()).let {
+                if (it == Z_OK) {
+                    log.info("deflate success")
+                } else {
+                    log.error("deflate error")
+                }
+            }
+            return outputData.getPointer(this).readBytes(outputLength.value.toInt())
+        }
+    }
+
+    actual fun inflate(data: ByteArray): ByteArray {
+        memScoped {
+            val outputLength = alloc<uLongfVar>().apply {
+                value = compressBound(data.size.convert()) * 2u
+            }
+            var outputData = allocArray<uBytefVar>(outputLength.value.toInt())
+            val inputData = data.toUByteArray().toCValues()
+            var flag=0
+            while(uncompress(outputData, outputLength.ptr, inputData, data.size.convert()).also { flag = it } == Z_BUF_ERROR){
+                outputLength.value *= 2u
+
+                val newOutputData = allocArray<uBytefVar>((outputLength.value * 2u).toInt())
+                memcpy(newOutputData, outputData, outputLength.value.convert())
+                free(outputData)
+                outputData = newOutputData
+            }
+
+            if (flag == Z_OK) {
+                log.info("inflate success")
+            } else {
+                log.error("inflate error")
+            }
+
+            return outputData.getPointer(this).readBytes(outputLength.value.toInt())
+        }
+    }
+}
