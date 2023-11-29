175c7c7bcf3651ac72d93198a0ebe2c5f7eb0a48
NickJi <nickji2019@outlook.com>
2023-11-28 21:29:37 -0500

    use certain server port
    Signed-off-by: NickJi <nickji2019@outlook.com>

--- server/src/main/kotlin/com/sbga/sdgbapp/Application.kt
+++ server/src/main/kotlin/com/sbga/sdgbapp/Application.kt
@@ -1,7 +1,6 @@
 package com.sbga.sdgbapp
 
 import Greeting
-import SERVER_PORT
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -9,7 +8,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
